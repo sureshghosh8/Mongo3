ae364a6662056b166059fd10275c564225e922c5
suresh <8SURESHGHOSH@GMAIL.COM>
2024-05-05 21:30:21 +0530

    create-routes

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const Listing=require("./models/listing.js");
 const path=require("path");
 const chat=require("./models/chat.js")
+
 app.set("viwes",path.join(__dirname,"views"));
 app.set("viwe engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main().then(()=>{console.log(" mongoose connection sucessful");})
 .catch(err => console.log(err));
@@ -16,9 +19,6 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-app.get("/",(req,res)=>{
-    res.send("root is working");
-});
 app.get("/testListing",async(req,res)=>{
   let sampleListing=new Listing({
     title:"my new villa",
@@ -47,6 +47,32 @@ app.get("/chats", async (req,res)=>{
   console.log(chats);
   res.render("index.ejs",{chats});
 });
+// new route
+app.get("/chats/new",(req,res)=>{
+  res.render("new.ejs");
+});
+//Create route
+app.post("/chats",(req,res)=>{
+  let{from,to,msg}=req.body;
+  let newChat= new chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
+  });
+  newChat.save()
+  .then((res)=>{
+    console.log("chat was saved");
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+  res.redirect("/chats");
+})
+app.get("/",(req,res)=>{
+  res.send("root is working");
+});
+
 app.listen(8080,()=>{
     console.log("server is connected (:")
 })
